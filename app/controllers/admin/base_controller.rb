868072c3c04c77f05c031e92ca721c430f419046
Evan Berg <evan.m.berg@gmail.com>
2017-11-29 22:24:54 -0800

    added fuck off flash for trying to access pages ya cant fuckin access ya dicks

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -6,6 +6,7 @@ class Admin::BaseController < ApplicationController
   # redirect if user not logged in or does not have a valid role
   def restrict_user_by_role
     unless current_user
+      flash[:notice] = 'GET THE FUCK OUTTA HERE'
       redirect_to root_path # change this to your 404 page if needed
     end
   end
